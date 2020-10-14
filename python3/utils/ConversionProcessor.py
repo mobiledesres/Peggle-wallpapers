6290ebb2fe8244c75eaa1fea9c82667fde99a7cc
mobiledesres <45989029+mobiledesres@users.noreply.github.com>
2020-10-14 08:56:04 -0700

    separate static method to get input files by extension

--- python3/utils/ConversionProcessor.py
+++ python3/utils/ConversionProcessor.py
@@ -10,33 +10,50 @@ class ConversionProcessor:
     convert_exec = 'opj_decompress'
 
     def __init__(self, input_dir: str):
-        self.__jp2_images = self.get_jp2_files(input_dir)
+        self.__jp2_images = self.__get_jp2_files(input_dir)
+        self.__jpg_images = self.__get_jpg_files(input_dir)
 
-    def get_jp2_files(self, input_dir: str):
+    @staticmethod
+    def get_files_by_ext(input_dir: str, ext: str) -> List[str]:
+        """
+        Get the collection of input files, by specified extension.
+        :param input_dir: Input directory.
+        :param ext: File extension. MUST START WITH '.'
+        :return: The list of files with specified extension (absolute paths)
+        """
+        input_files = glob.glob(join(input_dir, f'**/*{ext}'),
+                                recursive=True)
+        input_files = list(map(abspath, input_files))
+        input_files.sort()
+        return input_files
+
+    def __get_jp2_files(self, input_dir: str) -> List[str]:
         """
         Get the collection of jp2 files.
         :param input_dir: input directory.
         :return: the list of jp2 files.
         """
-        jp2_files_list = glob.glob(join(input_dir, '**/*.jp2'),
-                                   recursive=True)
-        jp2_files_list = list(map(abspath, jp2_files_list))
-        self.__jp2_images = jp2_files_list
-        return jp2_files_list
+        self.__jp2_images = self.get_files_by_ext(input_dir, '.jp2')
+        return self.__jp2_images
+
+    def __get_jpg_files(self, input_dir: str) -> List[str]:
+        self.__jpg_images = self.get_files_by_ext(input_dir, '.jpg')
+        return self.__jpg_images
 
     @staticmethod
-    def convert(input_file: str, output_file: str) -> int:
+    def __convert_from_jpeg2000(input_file: str, output_file: str) -> int:
         """
-        Read in a jpeg2000 image, and convert to another format.
-        Supported input formats: .j2k, .jp2, .j2c, .jpt
-        Supported output formats: .bmp, .pgm, .pgx, .png,  .pnm,  .ppm, .raw, .tga, .tif
+        Read in a jpeg2000 image, and convert it to another image type.
         See "man opj_decompress.1" for more details.
 
         :param input_file: Path of input file.
+            Valid input formats: .j2k, .jp2, .j2c, .jpt
         :param output_file: Path of output file.
+            Valid output formats: .bmp, .pgm, .pgx, .png,  .pnm,  .ppm, .raw, .tga, .tif
         :return: the return code of completed process.
         """
 
+        output_file = abspath(output_file)  # output absolute path in console
         return sp.run([ConversionProcessor.convert_exec, '-i', input_file,
                        '-o', output_file]).returncode
 
@@ -51,7 +68,8 @@ class ConversionProcessor:
 
         for jp2_image in self.__jp2_images:
             output_file = f'{join(output_dir, splitext(basename(jp2_image))[0])}{output_ext}'
-            process = Process(target=self.convert, args=(jp2_image, output_file))
+            process = Process(target=self.__convert_from_jpeg2000,
+                              args=(jp2_image, output_file))
             all_processes.append(process)
             process.start()
 
